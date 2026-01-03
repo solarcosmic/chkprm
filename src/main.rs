e8a7158defbaa2e3239d26fc94678b8d3405df4b
Solar <abe@solarcosmic.net>
2026-01-03 15:01:03 +1300

    Add more permission messages + user/group/other

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{env, fs};
+use std::{env};
 use std::fs::{metadata, File};
 use std::io::Read;
+use std::os::unix::fs::MetadataExt;
 //use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
-use fmodeparser::PermStrParser;
+use fmodeparser::{FullPermission, PermStrParser};
 
 fn file_content_read(file_path: &Path) -> std::io::Result<String> {
     let mut file = File::open(file_path)?;
@@ -13,9 +14,59 @@ fn file_content_read(file_path: &Path) -> std::io::Result<String> {
 }
 
 fn check_file_perms(file_path: &Path) -> std::io::Result<()> {
-    let meta = metadata(file_path)?;
-    let permission = meta.convert_permission_to_string().unwrap();
-    println!("Permissions: {permission}");
+    let meta = metadata(file_path);
+    let mut full_perm = FullPermission::new(meta?.mode()).unwrap();
+    let permission = metadata(file_path)?.convert_permission_to_string().unwrap();
+    let usr = full_perm.get_user().to_string();
+    let group = full_perm.get_group().to_string();
+    let other = full_perm.get_other().to_string();
+    //let mut usr_string = "No user permissions.";
+    //let mut group_string = "No group permissions.";
+    //let mut other_string = "No other permissions.";
+    // for user
+    if usr.chars().nth(0) == Some('r') && usr.chars().nth(1) == Some('w') && usr.chars().nth(2) == Some('x') {
+        println!("User: Full (read-write-execute), {usr} ({permission})");
+    } else if usr.chars().nth(0) == Some('r') && usr.chars().nth(1) == Some('w') {
+        println!("User: Modifiable (read-write), {usr} ({permission})");
+    } else if usr.chars().nth(0) == Some('r') {
+        println!("User: Read-only (read), {usr} ({permission})");
+    } else if usr.chars().nth(1) == Some('w') {
+        println!("User: Write-only (write), {usr} ({permission})");
+    } else if usr.chars().nth(2) == Some('x') {
+        println!("User: Executable-only (execute), {usr} ({permission})");
+    } else {
+        println!("No user permissions.");
+    }
+    // for group
+    if group.chars().nth(0) == Some('r') && group.chars().nth(1) == Some('w') && group.chars().nth(2) == Some('x') {
+        println!("Group: Full (read-write-execute), {usr} ({permission})");
+    } else if group.chars().nth(0) == Some('r') && group.chars().nth(1) == Some('w') {
+        println!("Group: Modifiable (read-write), {usr} ({permission})");
+    } else if group.chars().nth(0) == Some('r') {
+        println!("Group: Read-only (read), {usr} ({permission})");
+    } else if group.chars().nth(1) == Some('w') {
+        println!("Group: Write-only (write), {usr} ({permission})");
+    } else if group.chars().nth(2) == Some('x') {
+        println!("Group: Executable-only (execute), {usr} ({permission})");
+    } else {
+        println!("No group permissions.");
+    }
+    // for others
+    if other.chars().nth(0) == Some('r') && other.chars().nth(1) == Some('w') && other.chars().nth(2) == Some('x') {
+        println!("Other: Full (read-write-execute), {usr} ({permission})");
+    } else if other.chars().nth(0) == Some('r') && other.chars().nth(1) == Some('w') {
+        println!("Other: Modifiable (read-write), {usr} ({permission})");
+    } else if other.chars().nth(0) == Some('r') {
+        println!("Other: Read-only (read), {usr} ({permission})");
+    } else if other.chars().nth(1) == Some('w') {
+        println!("Other: Write-only (write), {usr} ({permission})");
+    } else if other.chars().nth(2) == Some('x') {
+        println!("Other: Executable-only (execute), {usr} ({permission})");
+    } else {
+        println!("No other permissions.");
+    }
+    //println!("Full permission: {}", usr.to_string());
+    //println!("Permissions: {permission}");
     Ok(())
 }
 
@@ -25,11 +76,15 @@ fn main() {
         return;
     }
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: chkprm <file_path>");
+        return;
+    }
     //dbg!(&args);
     //println!("{}", &args[1]);
     let path = Path::new(&args[1]);
     match file_content_read(path) {
-        Ok(content) => check_file_perms(path).unwrap(),
+        Ok(_content) => check_file_perms(path).unwrap(),
         Err(e) => eprintln!("Error: {}", e)
     }
     
